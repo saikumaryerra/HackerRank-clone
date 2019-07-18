fb03cea52e88f05542b5022f7a1f61b8a448ba28
saikumar <yerra.sai@mountblue.io>
2019-07-18 14:47:52 +0530

    unfinishedcommit for main page

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,5 @@ from . import views as view
 
 urlpatterns = [
     path('signup/', view.signup, name='signup'),
-    # path('login/', view.login, name='login'),
     path('',include('django.contrib.auth.urls'))
-    # path('logout/', view.logout, name='logout'),
 ]
