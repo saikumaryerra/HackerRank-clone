7dae668846e58e3e16971da53c6067e529f9a506
saikumar <yerra.sai@mountblue.io>
2019-07-22 11:10:07 +0530

    added models and connect in consumer

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Room, RoomMember, InterviewList
 
-# Register your models here.
+
+class InterviewListAdmin(admin.ModelAdmin):
+    list_display = ('interviewer', 'candidate', 'score', 'active')
+
+
+admin.site.register(InterviewList, InterviewListAdmin)
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('roomname', 'users')
+
+
+admin.site.register(Room, RoomAdmin)
+
+
+class RoomMemberAdmin(admin.ModelAdmin):
+    list_display = ('room', 'name')
+
+
+admin.site.register(RoomMember, RoomMemberAdmin)
--- main/consumers.py
+++ main/consumers.py
@@ -1,26 +1,79 @@
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 import json
+from .models import Room , RoomMember
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
+        try:
+            room = Room.objects.filter(roomname = self.room_group_name)[0]
+            if room.users < 2:
+                async_to_sync(self.channel_layer.group_add)(
+                self.room_group_name,
+                self.channel_name
+                )
+
+                roommember = RoomMember.objects.create(room = room)
+                roommember.name = self.channel_name
+                roommember.save()
+
+                room.users= room.users+1
+                room.save()
+                self.accept()                
+                self.send(text_data=json.dumps({
+                'type' : 'video_start',
+                'message': 'not initiator'
+                }))
+        except:
+            room = Room.objects.create()
+            async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
-        )
+            )
+            room.roomname = self.room_group_name
+            room.users = 1
+            roommember = RoomMember.objects.create(room = room)
+            roommember.name = self.channel_name
+            roommember.save()
+            # room.rand = a;
+            room.save()
+            self.accept()
+
+            self.send(text_data=json.dumps({
+            'type' : 'video_start',
+            'message': 'initiator'
+            }))
+
+        # Join room group
+        # async_to_sync(self.channel_layer.group_add)(
+        #     self.room_group_name,
+        #     self.channel_name
+        # )
 
         self.accept()
 
     def disconnect(self, close_code):
         # Leave room group
+        # room = Room.objects.filter(roomname = self.room_group_name)[0]
+        # if room.users > 1:
+        #     room.users = room.users-1
+        #     room.save()
+        # else :
+        #     room.delete()
+                
+        # roommember = RoomMember.objects.filter(room = room ,name = self.channel_name)[0]
+        # roommember.delete()
+        
         async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
+        
+        
+
 
     # Receive message from WebSocket
     def receive(self, text_data):
