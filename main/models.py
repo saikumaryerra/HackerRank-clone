ccfdc6f8c9c11f1a972bff41b52fde4730d1c4ed
saikumar <yerra.sai@mountblue.io>
2019-07-23 18:11:02 +0530

    pulled master

--- main/models.py
+++ main/models.py
@@ -1,36 +1,8 @@
 from django.conf import settings
 from django.db import models
-<<<<<<< HEAD
-from django.utils import timezone
-
-||||||| merged common ancestors
-=======
 from django.contrib.auth.models import User
 from datetime import datetime
->>>>>>> fe0b1464372731e4284092e6ea94555dfc9608bc
 
-<<<<<<< HEAD
-# Create your models here.
-class Interview(models.Model):
-    name = models.CharField(max_length = 30)
-    interviewer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
-    candidate = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
-    score = models.IntegerField(default=0)
-
-class Message(models.Model):
-    interview = models.ForeignKey(Interview , on_delete=models.CASCADE)
-    message = models.TextField()
-    sender = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
-    reciever = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
-    timestamp = models.DateTimeField(default=timezone.now)
-
-class Code(models.Model):
-    code = models.TextField()
-    interview = models.ForeignKey(Interview,on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(default=timezone.now)
-||||||| merged common ancestors
-# Create your models here.
-=======
 
 class InterviewList(models.Model):
    interviewer = models.ForeignKey(User, related_name= 'iv', on_delete=models.CASCADE)
@@ -51,4 +23,3 @@ class RoomMember(models.Model):
    room = models.ForeignKey(Room, on_delete=models.CASCADE)
 #    def __str__(self):
     #   return self.room
->>>>>>> fe0b1464372731e4284092e6ea94555dfc9608bc
